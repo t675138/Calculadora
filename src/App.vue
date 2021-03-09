ddbd192273768a366957df002bf81cfe11b140e6
t675138 <80212572+t675138@users.noreply.github.com>
2021-03-09 17:37:29 -0300

    Alteração do componentes para - Calculadora

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld/>
+    <CalculadoraNova/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import CalculadoraNova from './components/CalculadoraNova'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    CalculadoraNova
+
   }
 }
 </script>
---
+++ src/components/CalculadoraNova.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="Calculadora-Nova">
+        <h1> Calculadora Nova </h1>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CalculadoraNova'
+}
+</script>
+
+<style>
+
+</style>
